1be30412849b0b417accf1a911acc2866ab26c33
Joonas Karppinen <joonas.karppinen@metropolia.fi>
2024-08-28 11:09:07 +0300

    2.2 task4 finished

--- 2024_Syys/OOP/2.2/task5/GroceryListManager.java
+++ 2024_Syys/OOP/2.2/task5/GroceryListManager.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Set;
 import java.util.Map;
+import java.util.ArrayList;
 
 public class GroceryListManager{
 
@@ -43,18 +44,45 @@ public class GroceryListManager{
         System.out.println("Available items now:");
         list.displayAvailableItems();
 
+
+        // Shopping lists
+        // this is gonna be case sensitive, not gonna do that extra work
+        System.out.println("\n\n\nSHOPPING LIST, BABy!!");
+
+        list.createShoppingList("Tea Party", list.addItem("Tea", 1.5, "Drink", 2));
+        System.out.println("Item added: "
+                + list.addItemToList("Tea Party", list.addItem("Cookie", 2.5, "Sweets", 4)));
+
+        list.createShoppingList("Beer Party", list.addItem("Beer", 1.5, "Drink", 2));
+        System.out.println("Item added: "
+            + list.addItemToList("Beer Party", list.addItem("More Beer", 3.0, "Drink", 4)));
+        System.out.println("Item added: "
+            + list.addItemToList("Beer Party", list.addItem("Cookie", 2.5, "Sweets", 4)));
+
+        //list.displayShoppingList("Tea Party");
+        //list.displayShoppingList("Beer Party");
+        list.displayShoppingList();
+
+        // no cookies to beer party
+        System.out.println("Item removed: "
+            + list.removeItemFromList("Beer Party", "Cookie"));
+
+        System.out.println("\n\nNO COOKIES!! BEER ONLY!!");
+        list.displayShoppingList();
+
     }
 
     private HashMap<String, Grocery> groceryList = new HashMap<>();
 
     public void addItem(String s, Double d){ addItem(s, d, "Uncategorized"); }
     public void addItem(String s, Double d, String c){ addItem(s, d, c, 1); }
-    public void addItem(String s, Double d, String c, int q){
+    public Grocery addItem(String s, Double d, String c, int q){
         if (!checkItem(s) )
             groceryList.put(s.toLowerCase(), new Grocery(s, c, d, q));
         else
             groceryList.get(s.toLowerCase()).setQuantity(
                     groceryList.get(s.toLowerCase()).getQuantity()+q);
+        return groceryList.get(s.toLowerCase());
     }
 
     public boolean removeItem(String item){
@@ -117,7 +145,48 @@ public class GroceryListManager{
     }
     
 
+    // shopping lists
+    ArrayList<ShoppingList> shoppingLists = new ArrayList<>();
 
+    public void createShoppingList(String s){ shoppingLists.add(new ShoppingList(s)); }
+    public void createShoppingList(String s, Grocery g){
+        shoppingLists.add(new ShoppingList(s, g));
+    }
+
+    public boolean addItemToList(String s, Grocery g){
+        for(ShoppingList sl : shoppingLists)
+            if(sl.getName().equals(s))
+                return sl.addItem(g);
+        return false;
+    }
+
+    // not gonna implement returning of Grocery object to be able to use this
+    // just gonna leave this as a TODO
+    public boolean removeItemFromList(String s, Grocery g){
+        for(ShoppingList sl : shoppingLists)
+            if(sl.getName().equals(sl))
+                return sl.removeItem(g);
+        return false;
+    }
+
+    public boolean removeItemFromList(String s, String i){
+        for(ShoppingList sl : shoppingLists)
+            if(sl.getName().equals(s))
+                return sl.removeItem(i);
+        return false;
+    }
+
+    public void displayShoppingList(){
+        for (ShoppingList sl : shoppingLists)
+            sl.displayShoppingList();
+    }
+
+    // TODO implementation
+    // not gonna happen
+    public void displayShoppingList(String s){
+        for (ShoppingList sl : shoppingLists)
+            sl.displayShoppingList();
+    }
 
 
     // Grocery Item. An item in the shopping list. Would be made of a Product class
@@ -148,6 +217,45 @@ public class GroceryListManager{
         public double getPrice(){ return price; }
         public void setQuantity(int qty) { quantity = qty; }
         public int getQuantity(){ return quantity; }
+
+    }
+
+
+    class ShoppingList{
+
+        private String name;
+//        HashMap<String, Grocery> groceries;
+        private ArrayList<Grocery> groceries = new ArrayList<>();
+
+        ShoppingList(String name, Grocery g){ this(name); addItem(g); }
+        ShoppingList(String name){ this.name = name; }
+
+        public String getName(){ return name; }
+        
+        public boolean addItem(Grocery g){
+            return groceries.add(g);
+//            return false;
+        }
+
+        public boolean removeItem(String s){
+            //return removeItem(groceries.get(s.toLowerCase()));
+            for (Grocery g : groceries)
+                if (g.getName().equals(s))
+                    return removeItem(g);
+            return false;
+        }
+
+        public boolean removeItem(Grocery g){
+            return groceries.remove(g);
+        }
+
+        public ArrayList<Grocery> getShoppingList(){ return groceries; }
+
+        public void displayShoppingList(){
+            System.out.println("Shoppinglist " + name + ":");
+            for (Grocery g : groceries)
+                System.out.println(g.getName() + " - " + g.getQuantity() + " units");
+        }
 
     }
 
