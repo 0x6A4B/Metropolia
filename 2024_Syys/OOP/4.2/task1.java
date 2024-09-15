42aae73245b0ef5528aea3f92237f3aedf38890f
jaykay <0x6A4B@proton.me>
2024-09-15 22:02:50 +0300

    -_-

--- 2024_Syys/OOP/4.2/task1.java
+++ 2024_Syys/OOP/4.2/task1.java
@@ -1,23 +1,23 @@
 public class task1{
 
     public void main(String[] args){
-    int ii = 0;
-    Calculator calc = new Calculator();
+        int ii = 0;
+        Calculator calc = new Calculator();
 
-    while(calc.getValue() < 20)
-        calc.addInteger(ii++);
+        while(calc.getValue() < 20)
+            calc.addInteger(ii++);
 
-    calc.resetCalculator();
-    for (int i = 0; i < 5; i++)
-        calc.addInteger(i);
+        calc.resetCalculator();
+        for (int i = 0; i < 5; i++)
+            calc.addInteger(i);
 
-    // prompt: sum method that uses addInteger(int i) to add a number 5 times
-    Calculator calculator = new Calculator();
-    // Reset the calculator
-    calculator.resetCalculator();
-    // Calculate the sum
-    int result = calculator.sum();
-    System.out.println("The sum is: " + result);
+        // prompt: sum method that uses addInteger(int i) to add a number 5 times
+        Calculator calculator = new Calculator();
+        // Reset the calculator
+        calculator.resetCalculator();
+        // Calculate the sum
+        int result = calculator.sum();
+        System.out.println("The sum is: " + result);
 
     }
 
@@ -28,6 +28,7 @@ class Calculator{
     public void resetCalculator(){ value = 0; }
     public void addInteger(int i){ value += i; }
     public int getValue(){ return value; }
+
     public int sum() {
         for (int i = 0; i < 5; i++) {
             addInteger(1); // Add 1 to the value 5 times
