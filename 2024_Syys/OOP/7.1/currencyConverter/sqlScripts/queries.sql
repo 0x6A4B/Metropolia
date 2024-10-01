842b96b140e6522ba62677ba04bde85e707488e9
jaykay <0x6A4B@proton.me>
2024-10-01 23:35:09 +0300

    like => = for select

--- 2024_Syys/OOP/7.1/currencyConverter/sqlScripts/queries.sql
+++ 2024_Syys/OOP/7.1/currencyConverter/sqlScripts/queries.sql
@@ -1,5 +1,5 @@
 /* test queries as per assignment */
 SELECT * FROM currencies;
-SELECT * FROM currencies WHERE (abbr LIKE 'EUR');
+SELECT * FROM currencies WHERE (abbr = 'EUR');
 SELECT COUNT(*) FROM currencies;
 SELECT MAX(rate) FROM currencies;
