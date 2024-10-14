80b1f70f2e2fef2192219106c300c03d3e006ec6
jaykay <0x6A4B@proton.me>
2024-10-14 17:12:10 +0300

    rekursio ja stackoverflow ihmettelyä

--- 2024_Syys/OOP/8.4/tehtava2.java
+++ 2024_Syys/OOP/8.4/tehtava2.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class tehtava2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
 
         // stream with method reference and lambdas
-        System.out.println(list.stream().filter(tehtava2::isOdd).map(i -> i*2).reduce(0, (a,b) -> a+b));
+        System.out.println(list.stream().filter(tehtava2::isOdd).map(i -> i * 2).reduce(0, (a, b) -> a + b));
 
         // interfaces
         isOdd odd = (i) -> i % 2 != 0;
-        doubled dd = (i) -> i*2;
-        add ad = (i, j) -> i+j;
+        doubled dd = (i) -> i * 2;
+        add ad = (i, j) -> i + j;
 
         // ... this is not the way ...
         // using lambda for sum with foreach... just to avoid stream and use only lambda and functional interface
@@ -25,32 +25,67 @@ public class tehtava2 {
         // method reference with functional interfaces and integer.sum for reduce
         // but it's just the same stream filter map reduce
         System.out.println(list.stream().filter(odd::isOdd).map(dd::doubled).reduce(0, Integer::sum));
-                //reduce(0, (x, y) -> x + y));
+        //reduce(0, (x, y) -> x + y));
 
         // using recursive function
         System.out.println(recursiveAdd(list.toArray(new Integer[list.size()])));
+
+        // minkä takia "huonompi" taulukon kopionti ratkaisu kykenee käsittelemään suuremman
+        // taulukon ennen stackoverflowerror:ia kuin ratkaisu jossa taulukkoa ei kopioida?
+        Integer u[] = new Integer[10000];
+        Arrays.fill(u, 1);
+        for (int i = 0; i < 20; i++) {
+            try {
+                System.out.println("2: " + _recursiveAdd(u, u.length - 1));
+            } catch (StackOverflowError e) {
+                System.out.println("stack overflow2");
+            }
+            try {
+                System.out.println("1: " + recursiveAdd(u));
+            } catch (StackOverflowError e) {
+                System.out.println("stack overflow1");
+            }
+        }
+
     }
 
-    static boolean isOdd(int i){ return i % 2 != 0; }
+    static boolean isOdd(int i) {
+        return i % 2 != 0;
+    }
 
-    static int recursiveAdd(Integer[] a){
+    // kopioidaan taulukkoa => hitaampi
+    static int recursiveAdd(Integer[] a) {
         int sum = 0;
         if (a.length > 1)
             sum += recursiveAdd(Arrays.copyOfRange(a, 1, a.length));
         if (a[0] % 2 != 0)
-            return a[0]*2 + sum;
+            return a[0] * 2 + sum;
         return sum;
     }
 
+    // ei kopioida taulukkoa => nopeampi, mutta päätyy stack overflow:hun aiemmin?
+    static int _recursiveAdd(Integer[] a, int i) {
+        if (i == 0) {
+            if (a[0] % 2 != 0)
+                return a[0] * 2;
+        } else if (a[i] % 2 != 0)
+            return a[i] * 2 + _recursiveAdd(a, i - 1);
+
+        return 0;
+    }
+
 }
+
 @FunctionalInterface
 interface isOdd {
     boolean isOdd(int i);
 }
+
 interface doubled {
     int doubled(int i);
 }
-interface add{
+
+interface add {
     int add(int i, int j);
 }
 
