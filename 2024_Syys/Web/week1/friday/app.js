175d689e6c7385c9b76e2046dd6f04fde0af81a7
jaykay <0x6A4B@proton.me>
2024-10-28 10:10:36 +0200

    crud mockup

--- 2024_Syys/Web/week1/friday/app.js
+++ 2024_Syys/Web/week1/friday/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+//const bodyParser = require('body-parser')
 
-const { getRoot, getAPI } = require('./controllers.js'); // getRoot is imported
+const { getRoot, getProducts, createProduct, updateProduct, deleteProduct } = require('./controllers.js'); // getRoot is imported
 
 
 const port = 3005;
 
+/*
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+*/
+app.use(express.json())
+
+
 app.get('/', getRoot); // getRoot is used as a callback
-app.get('/API/name', getAPI);
+app.get('/API/product', getProducts);
+app.post('/API/product', createProduct);
+app.put('/API/product', updateProduct);
+app.delete('/API/product', deleteProduct);
+
 /*
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- 2024_Syys/Web/week1/friday/controllers.js
+++ 2024_Syys/Web/week1/friday/controllers.js
@@ -2,14 +2,45 @@
 const getRoot = (req, res) => {
     res.send('I am endpoint get /');
   };
-  
+
+products = [{id: 1, name: "product_1"}];
 
 
-const getAPI = (req, res) => {
-    console.log("XX")
-    console.log(req);
-    res.send('API_');
+const getProducts = (req, res) => {
+    console.log("req")
+    console.log(req.query.id);
+    let _result = "";
+    products.forEach(p => _result += "ID: " + p.id + " Name: " + p.name + "<br>")
+    //res.send(req.query.id);
+    console.log(_result);
+    res.send("RESULT:\n<br>" + _result);
+};
+
+const createProduct = (req, res) => {
+    console.log("create")
+    console.log(req.body)
+    products.push({ id: req.body.id, name: req.body.name });
+    res.send(req.body);
+};
+
+const updateProduct = (req, res) => {
+    console.log("update")
+    products[((products.filter(p => p.id = req.body.id))[0].id)-1]
+        = { id: req.body.id, name: req.body.name};
+    res.send(req.body);
+};
+
+const deleteProduct = (req, res) => {
+    console.log(products.filter(p => p.id == req.body.id));
+    console.log("delete: " + req.body.id)
+    
+    console.log(products.filter(p => p.id == req.body.id)[0]);
+    console.log(products.indexOf(products.filter(p => p.id == req.body.id)[0]));
+
+    if (products.indexOf(products.filter(p => p.id == req.body.id)[0], 1) >= 0)
+        products.splice(products.indexOf(products.filter(p => p.id == req.body.id)[0], 1));
+    res.send(req.body);
 };
 
 
-module.exports = { getRoot, getAPI };
+module.exports = { getRoot, getProducts, createProduct, updateProduct, deleteProduct };
