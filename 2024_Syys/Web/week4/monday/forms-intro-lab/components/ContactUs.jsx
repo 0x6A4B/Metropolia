0bce82863370583de64d39a0ec11e7cbde3cada3
jaykay <0x6A4B@proton.me>
2024-11-11 15:51:24 +0200

    monday forms-intro

--- 2024_Syys/Web/week4/monday/forms-intro-lab/components/ContactUs.jsx
+++ 2024_Syys/Web/week4/monday/forms-intro-lab/components/ContactUs.jsx
@@ -1,65 +1,76 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-
-const formData = {
-    name: "",
-    email: "",
-    phone: "",
-    phonetype: "",
-    comment: ""
-}
 
 function ContactUs() {
-    const [formDataState, setformDataState] = useState(formData);
-    console.log("_", formDataState);
-    
+  const [formDataState, setFormDataState] = useState({});
 
-    const changeHandler = (event) => {
-        let trgt = event.target;
-        console.log(trgt.id, trgt.value);
-        setformDataState(prevData => Object.assign(prevData, { [trgt.id] : trgt.value }))
-    }
+  console.log("_", formDataState);
 
-    const submitData = (event) => {
-        event.preventDefault();
-        let _formData = {...formDataState, submittedOn: new Date()};
-        console.log("Submit form: ", _formData);
-        setformDataState(formData);
-    }
-
-    return (
-      <div>
-        <h2>Contact Us</h2>
-        <form onSubmit={submitData}>
-          <div>
-            <label htmlFor='name' value={formDataState.name}>Name:</label>
-            <input id='name' type='text' onChange={changeHandler} />
-          </div>
-          <div>
-            <label htmlFor='email'>Email:</label>
-            <input id='email' type='text' onChange={changeHandler} />
-          </div>
-          <div>
-            <label htmlFor='phone'>Phone:</label>
-            <input id='phone' type='tel' onChange={changeHandler} />
-          </div>
-          <div>
-            <label htmlFor="phoneType">PhoneType:</label>
-            <select id="phonetype" onChange={changeHandler}>
-                <option value="">..Select phone type..</option>
-                <option value="mobile">Mobile</option>
-                <option value="landline">Landline</option>
-                <option value="tincan">Tincans connect with a string</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="comment">Comments</label>
-            <textarea id="comment" onChange={changeHandler} />
-          </div>
-          <button>Submit</button>
-        </form>
-      </div>
+  const changeHandler = (event) => {
+    let trgt = event.target;
+    //console.log(trgt.id, trgt.value);
+    setFormDataState(
+      (prevData) =>
+        ({ ...prevData, [trgt.id]: trgt.value })
+        /* Object.assign won't work!! Must use above syntax */
+        //Object.assign(formDataState, { ...prevData, [trgt.id]: trgt.value }) // THIS WON'T WORK!!!
     );
-  }
-  
-  export default ContactUs;
+
+    
+  };
+
+  const submitData = (event) => {
+    event.preventDefault();
+    let _formData = { ...formDataState, submittedOn: new Date() };
+    console.log("Submit form: ", _formData);
+
+    setFormDataState({});
+  };
+
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <form onSubmit={submitData}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            onChange={(e) => changeHandler(e)}
+            value={formDataState.name || ""}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="email" onChange={e => changeHandler(e)} value={formDataState.email || ""} />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            id="phone"
+            type="tel"
+            pattern="[0-9]{2,4}-[0-9]{7,8}"
+            onChange={e => changeHandler(e)}
+            value={formDataState.phone || ""}
+          />
+        </div>
+        <div>
+          <label htmlFor="phoneType">PhoneType:</label>
+          <select id="phonetype" onChange={e => changeHandler(e)} value={formDataState.phonetype || "..Select phone type.."}>
+            <option value="">..Select phone type..</option>
+            <option value="mobile">Mobile</option>
+            <option value="landline">Landline</option>
+            <option value="tincan">Tincans connect with a string</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="comment">Comments</label>
+          <textarea id="comment" onChange={e => changeHandler(e)} value={formDataState.comment || ""} />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactUs;
